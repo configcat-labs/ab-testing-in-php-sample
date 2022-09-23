4a8ec453c5be74ffe61d14e5315e67c251cd2140
Chavez Harris <codedbychavez@gmail>
2022-09-23 14:34:30 -0300

    Added initial code to sample app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ZSTRA Clothing</title>
+  <link rel="stylesheet" href="../css/app.css">
+</head>
+<body class="container">
+
+  <div class="flex navbar">
+ 
+      <nav class="flex navbar-left">
+        <a class="active" href="#">Women</a>
+        <a href="#">Men</a>
+        <a href="#">Kids</a>
+      </nav>
+
+    <div class="flex logo">
+      <a href="#">ZSTRA</a>
+    </div>
+
+      <nav class="flex navbar-right">
+        <a href="#">Search</a>
+        <a href="#">Account</a>
+        <a href="#">Cart(3)</a>
+      </nav>
+  
+  </div>
+
+  <div class="flex hero">
+    <h1>Ready to wear</h1>
+  </div>
+
+  <div class="flex cta">
+    <h2>Shop the latest trends with Zara's Just In Collection</h2>
+  </div>
+
+  <div class="flex searchbar">
+    <div class="filter">Filter</div>
+    <div class="sort">Sort By</div>
+  </div>
+
+  <div class="flex products">
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+    <div class="product-card">
+      <img src="" alt="">
+      <div class="product-details">
+        <p>Product Name</p>
+        <p>$24.99</p>
+      </div>
+    </div>
+
+
+  </div>
+
+  <div class="flex pagination">
+    <div>Prev</div>
+    <div>1 2 3 ... 10</div>
+    <div>Next</div>
+  </div>
+ 
+</body>
+
+<footer class="container">
+  <div class="flex footer-top">
+    <div>
+      <h4>Product</h4>
+      <nav>
+        <a href="#">BAZAR World</a>
+        <a href="#">Women</a>
+        <a href="#">Men</a>
+      </nav>
+    </div>
+    <div>
+      <h4>Company</h4>
+      <nav>
+        <a href="#">Fashion Shows</a>
+        <a href="#">Stories</a>
+        <a href="#">The House</a>
+        <a href="#">Work with us</a>
+      </nav>
+    </div>
+    <div>
+      <h4>Assistance</h4>
+      <nav>
+        <a href="#">Contact us</a>
+        <a href="#">Payment</a>
+        <a href="#">Delivery & Return</a>
+        <a href="#">FAQ</a>
+      </nav>
+    </div>
+  </div>
+  <div class="flex footer-bottom">
+    <div class="footer-social">
+      <h4>Follow us</h4>
+      <p>We are here for you</p>
+      <div class="social-icons">
+        <i>F</i>
+        <i>YT</i>
+        <i>IG</i>
+      </div>
+    </div>
+    <div class="footer-subscribe">
+      <h4>Subscribe for exclusive</h4>
+      <p>Your email address</p>
+    </div>
+    <div class="footer-preferences">
+      <div>Shopping for <span>Canada</span></div>
+      <div>Language <span>English</span></div>
+    </div>
+  </div>
+</footer>
+</html>
