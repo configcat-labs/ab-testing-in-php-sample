e963843fabf0b12aec2a17a109c418024e8953e7
Chavez Harris <codedbychavez@gmail>
2022-09-24 10:13:48 -0300

    Added assets and updated styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,164 +7,213 @@
   <title>ZSTRA Clothing</title>
   <link rel="stylesheet" href="../css/app.css">
 </head>
-<body class="container">
+<body>
+  <div class="container">
+    <div class="flex navbar">
 
-  <div class="flex navbar">
- 
       <nav class="flex navbar-left">
         <a class="active" href="#">Women</a>
         <a href="#">Men</a>
         <a href="#">Kids</a>
       </nav>
 
-    <div class="flex logo">
-      <a href="#">ZSTRA</a>
-    </div>
+      <div class="flex logo">
+        <a href="#">ZSTRA</a>
+      </div>
 
       <nav class="flex navbar-right">
         <a href="#">Search</a>
         <a href="#">Account</a>
         <a href="#">Cart(3)</a>
       </nav>
-  
+
+    </div>
+
+    <div class="flex hero">
+      <h1>Ready to wear</h1>
+    </div>
+
+    <div class="flex cta">
+      <h2>Shop the latest trends with ZSTRA's Just In Collection</h2>
+    </div>
+
+    <div class="flex searchbar">
+      <div class="filter">Filter</div>
+      <div class="sort">Sort By</div>
+    </div>
+
+    <div class="flex products">
+      <div class="product-card flex">
+        <img src="/images/image_1.png" alt="">
+        <div class="product-details">
+          <p class="product-description">ZW PREMIUM THE 90S</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_2.png" alt="">
+        <div class="product-details">
+          <p class="product-description">HEELED LEATHER SANDALS WITH PADDED STRAP</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_3.png" alt="">
+        <div class="product-details">
+          <p class="product-description">TWEED BLAZER</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_4.png" alt="">
+        <div class="product-details">
+          <p class="product-description">CUT OUT TOP</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_5.png" alt="">
+        <div class="product-details">
+          <p class="product-description">COMBINATION STRIPED SWEATER</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_6.png" alt="">
+        <div class="product-details">
+          <p class="product-description">SQUARE TOE HEELED ASYMMETRIC LEATHER SANDALS</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_7.png" alt="">
+        <div class="product-details">
+          <p class="product-description">CUT OUT TOP</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_8.png" alt="">
+        <div class="product-details">
+          <p class="product-description">ANIMAL PRINT CUT-OUT CROSSBODY BAG</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_9.png" alt="">
+        <div class="product-details">
+          <p class="product-description">PRINTED RUSTIC DRESS</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_10.png" alt="">
+        <div class="product-details">
+          <p class="product-description">WOVEN LEATHER HEELED TIE SANDALS</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_11.png" alt="">
+        <div class="product-details">
+          <p class="product-description">OVERSIZED BLAZER</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_12.png" alt="">
+        <div class="product-details">
+          <p class="product-description">REMOVABLE WALLET CORD DETAIL CROSSBODY BAG</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_13.png" alt="">
+        <div class="product-details">
+          <p class="product-description">SLOUCHY PANTS</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_14.png" alt="">
+        <div class="product-details">
+          <p class="product-description">LEATHER JACKET</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_15.png" alt="">
+        <div class="product-details">
+          <p class="product-description">HEELED LEATHER SQUARE TOE SANDALS</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+      <div class="product-card flex">
+        <img src="/images/image_16.png" alt="">
+        <div class="product-details">
+          <p class="product-description">ANIMAL PRINT TOTE BAG WITH CHAIN</p>
+          <div class="product-card-footer flex">
+            <button class="button view-more-button">View More</button>
+            <button class="button add-to-cart-button">Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 
-  <div class="flex hero">
-    <h1>Ready to wear</h1>
+  <div class="flex pagination container">
+    <a href="#">Prev</a>
+    <a href="#">1 2 3 ... 10</a>
+    <a href="#">Next</a>
   </div>
 
-  <div class="flex cta">
-    <h2>Shop the latest trends with Zara's Just In Collection</h2>
-  </div>
-
-  <div class="flex searchbar">
-    <div class="filter">Filter</div>
-    <div class="sort">Sort By</div>
-  </div>
-
-  <div class="flex products">
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-    <div class="product-card">
-      <img src="" alt="">
-      <div class="product-details">
-        <p>Product Name</p>
-        <p>$24.99</p>
-      </div>
-    </div>
-
-
-  </div>
-
-  <div class="flex pagination">
-    <div>Prev</div>
-    <div>1 2 3 ... 10</div>
-    <div>Next</div>
-  </div>
- 
 </body>
 
 <footer class="container">
@@ -200,20 +249,34 @@
     <div class="footer-social">
       <h4>Follow us</h4>
       <p>We are here for you</p>
-      <div class="social-icons">
-        <i>F</i>
-        <i>YT</i>
-        <i>IG</i>
+      <div class="social-icons flex">
+        <a href="#"><img src="./icons/square-facebook.svg" height="16" alt=""></a>
+        <a href="#"><img src="./icons/youtube.svg" height="20" alt=""></a>
+        <a href="#"><img src="./icons/instagram.svg" height="18" alt=""></a>
       </div>
     </div>
     <div class="footer-subscribe">
       <h4>Subscribe for exclusive</h4>
-      <p>Your email address</p>
+      <input type="text" name="email-address" id="email-address" placeholder="Your Email Address">
     </div>
     <div class="footer-preferences">
-      <div>Shopping for <span>Canada</span></div>
-      <div>Language <span>English</span></div>
+      <div>
+        <label for="shipping-top">Shipping to:</label>
+        <select name="shipping-to" id="shipping-top">
+          <option>Canada</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="language">Language:</label>
+        <select name="language" id="language">
+          <option>English</option>
+        </select>
+      </div>
     </div>
   </div>
+
 </footer>
+
+
 </html>
